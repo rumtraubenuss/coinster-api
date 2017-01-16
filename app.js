df3fe270423d5bbcce18559545d5bdd939870b0a
Bastian Schade <rumtraubenuss@googlemail.com>
2017-01-16 21:13:14 +0100

    Add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
@@ -8,6 +9,7 @@ const { DB_URI } = process.env;
 
 mongoose.connect(DB_URI);
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes);
