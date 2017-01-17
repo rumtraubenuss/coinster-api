83bab5c81723f28e03b48d4a0e950cc7785ea013
Bastian Schade <rumtraubenuss@googlemail.com>
2017-01-17 08:36:18 +0100

    Add limit and sort desc

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/prices').get((req, res) => {
-  CoinPrice.find({}, 'price_euro date', (err, prices) => {
-    if (err) { res.send(err) }
-    else {
-      res.json(prices);
-    }
-  });
+  CoinPrice.find({}, 'price_euro date')
+    .limit(50)
+    .sort('-date')
+    .exec((err, prices) => {
+      if (err) { res.send(err) }
+      else {
+        res.json(prices);
+      }
+    });
 });
 
 module.exports = router;
