8a7c4e3d1fe0327b987c85fb6cd794666ac12af2
Bastian Schade <rumtraubenuss@googlemail.com>
2017-04-11 23:30:42 +0200

    Add async await to route

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,22 @@ const CoinPrice = require('../models/coin-price');
 const express = require('express');
 const router = express.Router();
 
-router.route('/prices').get((req, res) => {
-  CoinPrice.distinct('type')
-    .then(types => {
-      const promises = types.sort().map(type => {
-        return (
-          CoinPrice.find({ type }, 'type price currency date')
-            .limit(1)
-            .sort('-date')
-            .exec()
-        );
-      });
-      return Promise.all(promises);
-    })
-    .then(prices => {
-      res.json(prices.map(res => res[0]));
-    })
-    .catch(er => {
-      res.send(er);
+router.route('/prices').get( async (req, res) => {
+  try {
+    const types = await CoinPrice.distinct('type')
+    const promises = types.sort().map(type => {
+      return (
+        CoinPrice.find({ type }, 'type price currency date')
+        .limit(1)
+        .sort('-date')
+        .exec()
+      );
     });
+    const prices = await Promise.all(promises);
+    res.json(prices.map(res => res[0]));
+  } catch(er) {
+    res.send(er);
+  }
 });
 
 module.exports = router;
