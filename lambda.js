7f1412fd052b5deff8556c3c2e255fcbe9aa6817
Bastian Schade <rumtraubenuss@googlemail.com>
2017-05-16 22:45:34 +0200

    Add first version of mongo fetch lambda function

---
+++ lambda.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+const { DB_API_KEY } = process.env;
+const moment = require('moment');
+
+const yesterday = moment().subtract(1, 'day');
+
+const query = JSON.stringify({
+  type: 'bitcoin',
+  $date: {
+    $gt: yesterday,
+  },
+});
+
+const host = 'https://api.mlab.com/api/1/databases/coinster/collections/prices';
+
+const url = `${host}?q=${query}&apiKey=${DB_API_KEY}`;
+
+fetch(url).then(res => res.json()).then(json => console.log(json));
