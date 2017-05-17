98ffae1618358ea9cd5d1ce6cd218cc9084b73a7
Bastian Schade <rumtraubenuss@googlemail.com>
2017-05-17 23:35:22 +0200

    Fix date query

--- lambda.js
+++ lambda.js
@@ -1,19 +1,15 @@
 const fetch = require('node-fetch');
 
 const { DB_API_KEY } = process.env;
+
 const moment = require('moment');
 
-const yesterday = moment().subtract(1, 'day');
+const yesterday = moment().subtract(1, 'day').toISOString();
 
-const query = JSON.stringify({
-  type: 'bitcoin',
-  $date: {
-    $gt: yesterday,
-  },
-});
+const query = `{"type": "bitcoin", "date": {"$gt": {"$date": "${yesterday}"} }}`;
 
 const host = 'https://api.mlab.com/api/1/databases/coinster/collections/prices';
 
 const url = `${host}?q=${query}&apiKey=${DB_API_KEY}`;
 
-fetch(url).then(res => res.json()).then(json => console.log(json));
+fetch(url).then(res => res.json()).then(json => console.log({ bitcoin: json }));
