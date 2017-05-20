29724109ec508b65b0d9cc3b75f851fc0fb0b996
Bastian Schade <rumtraubenuss@googlemail.com>
2017-05-20 12:01:07 +0200

    Fetch coins in parralel

--- lambda.js
+++ lambda.js
@@ -1,28 +1,29 @@
 const fetch = require('node-fetch');
-
 const { DB_API_KEY } = process.env;
-
 const moment = require('moment');
-
 const yesterday = moment().subtract(1, 'day').toISOString();
-
-const query = JSON.stringify({
-  type: 'bitcoin',
-  date: {
-    $gt: {
-      $date: yesterday,
-    },
-  },
-});
-
+const host = 'https://api.mlab.com/api/1/databases/coinster/collections/prices';
 const fields = JSON.stringify({
   _id: 0,
   price_raw: 0,
   currency: 0,
 });
 
-const host = 'https://api.mlab.com/api/1/databases/coinster/collections/prices';
+function getUrl(currency) {
+  const query = JSON.stringify({
+    type: currency,
+    date: {
+      $gt: {
+        $date: yesterday,
+      },
+    },
+  });
+  return `${host}?f=${fields}&q=${query}&apiKey=${DB_API_KEY}`;
+}
 
-const url = `${host}?f=${fields}&q=${query}&apiKey=${DB_API_KEY}`;
+const promises = ['bitcoin', 'ethereum'].map(currency => (
+  fetch(getUrl(currency)).then(res  => res.json())
+));
 
-fetch(url).then(res => res.json()).then(json => console.log({ bitcoin: json }));
+Promise.all(promises)
+  .then(([bitcoin, ethereum]) => console.log({ bitcoin, ethereum }));
